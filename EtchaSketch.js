96bcf0b6225e4db0718b419becc7602db84f6bd2
Sanjai Kumar Mani <sanjaikumar.mani@gmail.com>
2021-11-08 20:02:55 +0530

    Add more EaS and index.html and css too

--- EtchaSketch.js
+++ EtchaSketch.js
@@ -1,13 +1,22 @@
 let gridBody = document.querySelector('#gridBody');
 let gridRow;
 let gridColumn;
-for (let i=0;i<16;i++){
+for (let i=0;i<64;i++){
     gridRow=document.createElement('div');
     gridBody.appendChild(gridRow);
     gridRow.classList.add(`Row`);
-    for (let j=0;j<16;j++){
+    gridRow.setAttribute('id',`Row${i+1}`)
+    for (let j=0;j<64;j++){
         gridColumn=document.createElement('div');
         gridRow.appendChild(gridColumn);
         gridColumn.classList.add('column');
+        gridColumn.addEventListener('mouseenter',changeColor);
+        gridColumn.setAttribute('id',`Column${j+1} Row${i+1}`);
     }
 }
+function changeColor(e){
+    console.log(e.target)
+    e.target.style.backgroundColor='white'
+}
+
+
