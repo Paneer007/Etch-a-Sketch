84f6f6c9df070383db74f719bc3f02ff6b89c229
Sanjai Kumar Mani <sanjaikumar.mani@gmail.com>
2021-11-08 22:29:06 +0530

    Made changes to js to make the size rechangable

--- EtchaSketch.js
+++ EtchaSketch.js
@@ -1,12 +1,43 @@
+function change(boxes,new_padding){
+    var newPB=document.querySelector('#parentOfGridBody');
+    newPB.removeChild(gridBody);
+    gridBody=document.createElement('div');
+    gridBody.classList.add='gridBody';
+    newPB.appendChild(gridBody);
+    for (let i=0;i<boxes;i++){
+        gridRow=document.createElement('div');
+        gridBody.appendChild(gridRow);
+        gridRow.classList.add(`row`);
+        gridRow.setAttribute('id',`Row${i+1}`)
+        for (let j=0;j<boxes;j++){
+            gridColumn=document.createElement('div');
+            gridRow.appendChild(gridColumn);
+            gridColumn.classList.add('column');
+            gridColumn.addEventListener('mouseenter',changeColor);
+            gridColumn.setAttribute('id',`Column${j+1} Row${i+1}`);
+            gridColumn.setAttribute('style',`padding: ${new_padding}px`)
+        }
+    }
+}
+function adjustFiness(event){
+    let boxes =event.target.value;
+    let padding_factor= boxes/16;
+    let new_padding= 20/padding_factor;
+    console.log(new_padding)
+    change(boxes,new_padding);
+}
+let parentBody=document.querySelector('#parentOfGridBody');
+let slider= document.querySelector('.slider');
+slider.addEventListener('change',adjustFiness)
 let gridBody = document.querySelector('#gridBody');
 let gridRow;
 let gridColumn;
-for (let i=0;i<64;i++){
+for (let i=0;i<16;i++){
     gridRow=document.createElement('div');
     gridBody.appendChild(gridRow);
-    gridRow.classList.add(`Row`);
+    gridRow.classList.add(`row`);
     gridRow.setAttribute('id',`Row${i+1}`)
-    for (let j=0;j<64;j++){
+    for (let j=0;j<16;j++){
         gridColumn=document.createElement('div');
         gridRow.appendChild(gridColumn);
         gridColumn.classList.add('column');
@@ -18,5 +49,3 @@ function changeColor(e){
     console.log(e.target)
     e.target.style.backgroundColor='white'
 }
-
-
